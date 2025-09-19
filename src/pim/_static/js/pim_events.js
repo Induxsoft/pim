016b130e1341b7a8a45906646c8a476e753cb3d6
Francisco Ávila <50455756+7oni3c@users.noreply.github.com>
2025-09-19 17:28:51 -0600

    update (events): Validar inicio y duración del evento
    Nuevo servicio para validar rango de tiempo del evento a agendar.

--- src/pim/_static/js/pim_events.js
+++ src/pim/_static/js/pim_events.js
@@ -1,11 +1,44 @@
 var events = {
-    formId:"", form:null, ff:null,
+    form_id:"", form:null, ff:null,
     url_bitacora:"",
 
     init()
     {
-        tools.trigger('select[id="type"]','change');
+        this.form = document.getElementById(this.form_id);
+        this.ff = this.form.elements;
         this.setLog()
+
+        const type = this.ff['type'];
+        const start = this.ff['start'];
+        const duration = this.ff['duration'];
+        const backcolor = this.ff['backcolor'];
+        const color = this.ff['color'];
+
+        type.addEventListener('change', (e) => {
+            backcolor.value = e.target.selectedOptions[0].dataset.backcolor;
+            color.value = e.target.selectedOptions[0].dataset.color;
+        });
+        start.addEventListener('change', (e) => this.checkStartRange());
+        duration.addEventListener('change', (e) => this.checkStartRange());
+
+        tools.trigger(type,'change');
+    },
+
+    checkStartRange()
+    {
+        let url = "/!/pim/events/?action=check-start-range";
+        url += "&calendar="+this.ff['calendar'].value;
+        url += "&start="+this.ff['start'].value;
+        url += "&duration="+this.ff['duration'].value;
+
+        InduxsoftCrudlModel.InvokeService(url, null,
+            (resp) => {},
+            (error) => {
+                if (error.message) alert(error.message);
+                else console.error(error);
+            },
+            "GET", false
+        );
     },
 
     setLog()
